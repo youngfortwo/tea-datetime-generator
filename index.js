1e6292e8b2d94b66ce012ba822cb965fa5079f11
youngfortwo <36065362+youngfortwo@users.noreply.github.com>
2024-03-31 12:34:31 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,20 @@
-function hello(name) {
+function index(name) {
     return 'hello ' + name + ', now is ' + Date();
 }
+function generateFormattedDate(format) {
+  const currentDate = new Date();
+  const options = {
+    year: 'numeric',
+    month: '2-digit',
+    day: '2-digit',
+    hour: '2-digit',
+    minute: '2-digit',
+    second: '2-digit',
+    timeZoneName: 'short'
+  };
+  return new Intl.DateTimeFormat('en-US', options).format(currentDate);
+}
 module.exports = {
-    hello
+    hello,
+    generateFormattedDate
 }
